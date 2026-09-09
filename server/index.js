248dc21b4a591be0eec3c3f38f07a7bd66a38595
Aryan Nair <aryanyouofficial3557u@gmail.com>
2026-09-09 22:02:45 +0530

    token-setup

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 6000
 
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.json({
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
         required:true,
     },
     credits:{
-        type:Number,
+        type:Number,   
         default:100
     }
 }, {timestamps:true})
