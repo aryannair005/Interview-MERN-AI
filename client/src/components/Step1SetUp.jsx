1cc3752c0ac187c60ef86eb6f5dc429e76eb9531
Aryan Nair <aryanyouofficial3557u@gmail.com>
2026-09-19 11:45:22 +0530

    Client-Interview-Step1-to-Step2

--- client/src/components/Step1SetUp.jsx
+++ client/src/components/Step1SetUp.jsx
@@ -10,8 +10,12 @@ import {
   FaChartLine
 } from "react-icons/fa"
 import { ServerUrl } from '../App.jsx'
+import { useDispatch, useSelector } from 'react-redux'
+import { setUserData } from '../redux/userSlice.js'
 
 const Step1SetUp = ({onStart}) => {
+  const {userData} =useSelector((state)=>state.user)
+  const dispatch = useDispatch()
   const [role,setRole] = useState("")
   const [experience,setExperience] = useState("")
   const [mode,setMode] = useState("Technical")
@@ -43,6 +47,22 @@ const Step1SetUp = ({onStart}) => {
       setAnalysisDone(true) 
     }catch(error){
       console.log(error)
+    }
+  }
+
+  const handleStart = async () => {
+    setLoading(true)
+    try {
+      const result = await axios.post(ServerUrl + "/api/interview/generate-questions",{role,experience,mode,resumeText,projects,skills},{withCredentials:true})
+      console.log(result.data)
+      if(userData){
+        dispatch(setUserData({...userData,credits:result.data.creditsLeft}))
+      }
+      setLoading(false)
+      onStart(result.data)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
     }
   }
 
@@ -180,13 +200,13 @@ const Step1SetUp = ({onStart}) => {
                 )}
               </motion.div>
             )}
-            <motion.button 
-            disabled= {!role || !experience }
+            <motion.button  onClick={handleStart}
+            disabled= {!role || !experience || loading}
             whileHover={{scale:1.03}}
             whileTap={{scale:0.95}}
             className='w-full disabled:bg-gray-600 bg-green-600 hover:bg-green-700 text-white py-3 rounded-full text-lg font-semibold transition duration-300 shadow-md'
             >
-              Start Interview
+              {loading ? "Starting...":"Start Interview"}
             </motion.button>
           </div>
         </motion.div>
